98f548b08012fe39fa5dca5622973cfaaaa31550
Tom Pelsmaeker <tlpelsmaeker@gmail.com>
2015-11-03 18:17:32 +0100

    Help

---
+++ app/src/main/java/com/example/gemini/mr_potatohead/PotatoheadMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gemini.mr_potatohead;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PotatoheadMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_potatohead_main);
+    }
+}
